a7b91d143c6958ac7eab8d82542818d2a00f6f9c
kalmari <kohdicarr@gmail.com>
2019-11-02 23:49:37 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,105 +1,101 @@
 use goblin::Object;
-use libc::{c_char, c_int};
-use path_dsl::path;
-use std::{ffi::CString, fs::File, io::Read, ptr};
+use libc::{c_char, c_int, dlsym, ENOEXEC};
+use null_terminated::{Nul, NulStr};
+use std::{
+    collections::{HashMap, VecDeque},
+    env,
+    fs::File,
+    io::Read,
+    mem,
+    path::PathBuf,
+    ptr,
+};
 
-mod args;
-mod env;
-mod path;
+#[no_mangle]
+pub extern "C" fn execve(path: &NulStr, args: &Nul<&NulStr>, env: &Nul<&NulStr>) -> c_int {
+    let path = path.to_string().into();
+    let args = unsafe { std::slice::from_raw_parts(args.as_ptr(), args.len()) }
+        .iter()
+        .map(|s| s.to_string())
+        .collect();
+    let env = unsafe { std::slice::from_raw_parts(env.as_ptr(), env.len()) }
+        .iter()
+        .map(|s| {
+            let s = s.to_string();
+            let v: Vec<&str> = s.splitn(2, '=').collect();
 
-pub use args::Args;
-pub use env::Env;
-pub use path::Path;
+            (v[0].to_string(), v[1].to_string())
+        })
+        .collect();
 
-pub fn real_execve(
-    path: *const c_char,
-    argv: *const *const c_char,
-    envp: *const *const c_char,
-) -> c_int {
-    unsafe {
-        let sym = b"execve\0";
-        let raw = libc::dlsym(libc::RTLD_NEXT, sym.as_ptr() as *const _);
-        let fun = std::mem::transmute::<
-            *const _,
-            unsafe extern "C" fn(
-                *const c_char,
-                *const *const c_char,
-                *const *const c_char,
-            ) -> c_int,
-        >(raw);
-        fun(path, argv, envp)
+    match execve2(path, args, env) {
+        Ok(ret) => ret,
+        Err(_) => ENOEXEC,
     }
 }
 
-#[no_mangle]
-pub extern "C" fn execve(path: Path, args: Args, env: Env) -> c_int {
-    let path = path.to_path();
-    let mut args = args.to_vec();
-    let mut env = env.to_map();
+fn execve2(
+    mut path: PathBuf,
+    mut args: VecDeque<String>,
+    mut env: HashMap<String, String>,
+) -> failure::Fallible<c_int> {
+    let mut file = File::open(&path)?;
+    let mut buf = Vec::new();
 
-    let mut file = match File::open(&path) {
-        Ok(file) => file,
-        Err(_) => return libc::EACCES,
+    file.read_to_end(&mut buf)?;
+
+    let elf = if let Object::Elf(elf) = Object::parse(&buf)? {
+        elf
+    } else {
+        return Ok(ENOEXEC);
     };
 
-    let mut buffer = Vec::new();
+    let arch = match elf.header.e_machine {
+        0x03 => "i386",
+        0x3e => "x86_64",
+        0x28 => "arm",
+        0xb7 => "aarch64",
+        _ => return Ok(ENOEXEC),
+    };
 
-    match file.read_to_end(&mut buffer) {
-        Err(_) => return libc::EACCES,
-        _ => (),
+    if arch != env::consts::ARCH {
+        let qemu = which::which(format!("qemu-{}", arch))?;
+
+        env.insert("QEMU_ARGV0".to_string(), args.pop_front().unwrap());
+        args.push_front(path.display().to_string());
+
+        path = qemu;
     }
 
-    let arch = match Object::parse(&buffer) {
-        Ok(Object::Elf(elf)) => match elf.header.e_machine {
-            0x03 => "x86",
-            0x3E => "x86_64",
-            0x28 => "arm",
-            0xB7 => "aarch64",
-            _ => return libc::ENOEXEC,
-        },
-        // Handle shebangs
-        // Err(_) =>
-        _ => return libc::ENOEXEC,
-    };
+    Ok(execve3(path, args, env))
+}
 
-    let path = if arch == std::env::consts::ARCH {
-        path
-    } else {
-        let sysroot = path!("opt" | "pmbm" | arch);
-        let qemu =
-            which::which(format!("qemu-{}-static", arch)).expect("please install qemu-user-static");
-        let qemu_ld_prefix = path!(sysroot | "lib");
+fn execve3(path: PathBuf, mut args: VecDeque<String>, env: HashMap<String, String>) -> c_int {
+    let path = format!("{}\0", path.display());
+    let path = path.as_ptr();
 
-        args.insert(0, format!("{}", qemu.display()));
+    for arg in &mut args {
+        arg.push('\0');
+    }
 
-        env.remove("LD_PRELOAD");
-        env.insert(
-            "QEMU_LD_PREFIX".to_owned(),
-            format!("{}", qemu_ld_prefix.into_pathbuf().display()),
-        );
-
-        qemu
-    };
-
-    let path = CString::new(format!("{}", path.display())).unwrap();
-
-    let args: Vec<_> = args
-        .into_iter()
-        .map(|arg| CString::new(arg).unwrap())
-        .collect();
-
-    let mut args: Vec<_> = args.iter().map(|arg| arg.as_ptr()).collect();
-
+    let mut args: Vec<_> = args.iter().map(|a| a.as_ptr()).collect();
     args.push(ptr::null());
+    let args = args.as_ptr();
 
-    let env: Vec<_> = env
-        .into_iter()
-        .map(|(key, value)| CString::new(format!("{}={}", key, value)).unwrap())
-        .collect();
-
-    let mut env: Vec<_> = env.iter().map(|env| env.as_ptr()).collect();
-
+    let env: Vec<_> = env.iter().map(|(k, v)| format!("{}={}\0", k, v)).collect();
+    let mut env: Vec<_> = env.iter().map(|e| e.as_ptr()).collect();
     env.push(ptr::null());
+    let env = env.as_ptr();
 
-    real_execve(path.as_ptr(), args.as_ptr(), env.as_ptr())
+    unsafe {
+        let sym = dlsym(-1i64 as *mut _, b"execve\0".as_ptr() as *const _);
+        let fun = mem::transmute::<
+            *const _,
+            Option<
+                extern "C" fn(*const c_char, *const *const c_char, *const *const c_char) -> c_int,
+            >,
+        >(sym);
+
+        fun.unwrap()(path, args, env)
+    }
 }
